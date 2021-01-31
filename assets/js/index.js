8442a0e33f6f9f9302d2f904fc0f4591ce9b6e5f
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-01 00:04:11 +0200

    Add main functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,13 @@
 new URL('https://www.facebook.com/DwayneJohnson'); // {hostname}
 new Map().set('www.facebook.com', 'src to fb icon'); // key - hostname
 
+
+const cardContainer = document.getElementById('root');
+
+const cards = responseData.map((place) => createPlaceCardElements(place));
+
+cardContainer.append(...cards);
+
 /**
  * @param {string} type
  * @param {object} options
@@ -17,56 +24,69 @@ function createElement(type, {classNames, onClick}, children) {
     return elem;
 }
 
-const cardContainer = document.getElementById('root');
-
-const HTMLLIElem = responseData.map((place) =>
-    createPlaceCardElements(place));
-
-cardContainer.append(...HTMLLIElem);
-
 function createPlaceCardElements(place) {
-    const wrapper = document.createElement('li');
-    wrapper.classList.add('cardWrapper');
-    const article = document.createElement('article');
-    article.classList.add('cardContainer');
-    wrapper.append(article);
+    const {firstName} = place;
+    const wrapper = createElement('li', {classNames: ['cardWrapper']},
+        createElement('article', {classNames: ['cardContainer']}, [
+            createCardImage(place),
+            createElement('h2', {classNames: ['cardName']}, [
+                document.createTextNode(firstName),
+            ]),
+        ]));
 
-    const div = document.createElement('div');
-    div.classList.add('imageWrapper');
-    div.style.backgroundColor = stringToColour(`${place.firstName}`);
-    const divElem = document.createElement('div');
-    divElem.classList.add('imagePlaceholder', 'imagePlacement');
-    const divElementContent = place.firstName[0];
-    divElem.append(document.createTextNode(divElementContent));
-
-
-    const imgPlace = document.createElement('img');
-    imgPlace.classList.add('cardImage', 'imagePlacement');
-    imgPlace.setAttribute('src', place.profilePicture);
-    imgPlace.setAttribute('alt', place.lastName)
-    imgPlace.addEventListener('error', imageErrorHandler);
-
-    div.append(divElem, imgPlace);
-
-    const heading3 = document.createElement('h3');
-    heading3.classList.add('cardName');
-    heading3.append(document.createTextNode(place.firstName));
+    // const wrapper = document.createElement('li');
+    // wrapper.classList.add('cardWrapper');
+    //
+    // const article = document.createElement('article');
+    // article.classList.add('cardContainer');
+    //
+    // const heading3 = document.createElement('h3');
+    // heading3.classList.add('cardName');
+    // heading3.append(document.createTextNode(firstName));
     // const heading4 = document.createElement('h4');
     // heading4.classList.add('cardSpecialty');
     // const par = document.createElement('p');
     // par.classList.add('cardDescription');
 
-    article.append(div, heading3);
-
+    // article.append(createCardImage(place), heading3);
+    // wrapper.append(article);
     return wrapper;
 }
 
-// EVENT LISTENERS
 
+function createCardImage(place) {
+    const {firstName, lastName} = place;
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('imageWrapper');
+    imageWrapper.style.backgroundColor = stringToColour(firstName);
+    const initials = document.createElement('div');
+    initials.classList.add('imagePlaceholder', 'imagePlacement');
+
+    initials.append(document.createTextNode(firstName[0] || ''));
+    createImage(place);
+    imageWrapper.append(initials);
+    return imageWrapper;
+}
+
+function createImage({profilePicture, lastName}) {
+    const image = document.createElement('img');
+    image.classList.add('cardImage', 'imagePlacement');
+    image.setAttribute('src', profilePicture);
+    image.setAttribute('alt', lastName)
+    image.addEventListener('error', imageErrorHandler);
+    image.addEventListener('load', imageLoadHandler);
+}
+
+// EVENT LISTENERS
 function imageErrorHandler({target}) {
     target.remove();
 }
 
+function imageLoadHandler({target}) {
+
+}
+
+// UTILS
 function stringToColour(str) {
     let i;
     let hash = 0;
