0feea6cb03ced940424c5b7dd17f8f7194640580
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-01 17:02:27 +0200

    Add corrected code

---
+++ assets/js/reg.js
@@ -0,0 +1,9 @@
+
+
+function checkValidateName(name) {
+    const reg = new RegExp('^[A-Z][a-z]* [A-Z][a-z]*$', 'gmi');
+    if (reg.test(name)) {
+        return 'success';
+    } return 'error';
+}
+
