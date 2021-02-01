17db6ae0ba3df4d99f0420c3852a1f953ed4cea2
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-01 02:10:41 +0200

    Add refactor code

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,17 @@ const cards = responseData.map((place) => createPlaceCardElements(place));
 
 cardContainer.append(...cards);
 
+
+
+function createPlaceCardElements(place) {
+    const { firstName } = place;
+    const wrapper = createElement('li', { classNames: ['cardWrapper'] }, [
+        createCardImage(place), createElement('article', { classNames: ['cardContainer'] }, [
+            createCardImage(place), createElement('h2', { classNames: ['cardName'] }, [
+                document.createTextNode(firstName)])])]);
+    return wrapper;
+};
+
 /**
  * @param {string} type
  * @param {object} options
@@ -16,75 +27,46 @@ cardContainer.append(...cards);
  * @param {function} options.onClick
  * @param {HTMLElement[]} children
  */
-function createElement(type, {classNames, onClick}, children) {
+function createElement(type, { classNames, onClick }, children) {
     const elem = document.createElement(type);
     elem.classList.add(...classNames);
     elem.onclick = onClick;
     elem.append(...children);
     return elem;
-}
-
-function createPlaceCardElements(place) {
-    const {firstName} = place;
-    const wrapper = createElement('li', {classNames: ['cardWrapper']},
-        createElement('article', {classNames: ['cardContainer']}, [
-            createCardImage(place),
-            createElement('h2', {classNames: ['cardName']}, [
-                document.createTextNode(firstName),
-            ]),
-        ]));
-
-    // const wrapper = document.createElement('li');
-    // wrapper.classList.add('cardWrapper');
-    //
-    // const article = document.createElement('article');
-    // article.classList.add('cardContainer');
-    //
-    // const heading3 = document.createElement('h3');
-    // heading3.classList.add('cardName');
-    // heading3.append(document.createTextNode(firstName));
-    // const heading4 = document.createElement('h4');
-    // heading4.classList.add('cardSpecialty');
-    // const par = document.createElement('p');
-    // par.classList.add('cardDescription');
-
-    // article.append(createCardImage(place), heading3);
-    // wrapper.append(article);
-    return wrapper;
-}
-
+};
 
 function createCardImage(place) {
-    const {firstName, lastName} = place;
+    const { firstName, id } = place;
     const imageWrapper = document.createElement('div');
+    imageWrapper.setAttribute('id', `wrapper${id}`);
     imageWrapper.classList.add('imageWrapper');
     imageWrapper.style.backgroundColor = stringToColour(firstName);
     const initials = document.createElement('div');
     initials.classList.add('imagePlaceholder', 'imagePlacement');
-
     initials.append(document.createTextNode(firstName[0] || ''));
-    createImage(place);
-    imageWrapper.append(initials);
+    imageWrapper.append(initials, createImage(place));
     return imageWrapper;
-}
+};
 
-function createImage({profilePicture, lastName}) {
+function createImage({ profilePicture, lastName, id}) {
     const image = document.createElement('img');
     image.classList.add('cardImage', 'imagePlacement');
     image.setAttribute('src', profilePicture);
-    image.setAttribute('alt', lastName)
+    image.setAttribute('alt', lastName);
+    image.dataset.id = id;
     image.addEventListener('error', imageErrorHandler);
-    image.addEventListener('load', imageLoadHandler);
-}
+    // image.addEventListener('load', imageLoadHandler);
+    return image;
+};
 
 // EVENT LISTENERS
-function imageErrorHandler({target}) {
+function imageErrorHandler({ target }) {
     target.remove();
-}
+};
 
-function imageLoadHandler({target}) {
-
-}
+function imageLoadHandler({ target: { dataset: { id }}, target }) {
+    document.getElementById(`wrapper${id}`).append(target);
+};
 
 // UTILS
 function stringToColour(str) {
@@ -99,4 +81,4 @@ function stringToColour(str) {
         colour += ('00' + value.toString(16)).substr(-2);
     }
     return colour;
-}
+};
