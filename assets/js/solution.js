b55cd2d724470da3e8ba19e9efd1f28b25ec0817
Umar Khalilov <ERMASTER100@gmail.com>
2021-02-03 16:59:39 +0200

    Add refactor code

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -2,45 +2,45 @@
 /* new URL('https://www.facebook.com/DwayneJohnson'); // {hostname}
 new Map().set('www.facebook.com', 'src to fb icon'); // key-hostname */
 
+const socNetworks = new Map(
+    [['www.facebook.com', './assets/icons/facebook.svg'],
+    ['twitter.com', './assets/icons/twitter.svg'],
+    ['www.instagram.com', './assets/icons/instagram.svg']]
+);
+
 const cardContainer = document.getElementById('root');
 
-const cards = responseData.map((place) => createPlaceCardElements(place));
+const cards = responseData.map((user) => createPlaceCardElements(user));
 
 cardContainer.append(...cards);
 
-function createPlaceCardElements(place) {
-    const { firstName, lastName, description, specialty } = place;
+function createPlaceCardElements(user) {
+    const { firstName, lastName, description, specialty } = user;
     return createElement('li', { classNames: ['cardWrapper'] }, [
         createElement('article', { classNames: ['cardContainer'] }, [
-            createCardImage(place), createElement('h2', { classNames: ['cardName'] }, [
+            createCardImage(user), createElement('h2', { classNames: ['cardName'] }, [
                 document.createTextNode(`${firstName} ${lastName}` || '')]),
             createElement('h3', { classNames: ['cardSpecialty'] }, [
                 document.createTextNode(specialty || '')]),
             createElement('p', { classNames: ['cardDescription'] }, [
                 document.createTextNode(description || '')]),
             createElement('div', {
-                classNames: ['cardLink'][createLinkIcons(['twitter.com',
-                    'www.facebook.com',
-                    'www.instagram.com'])]
-            })
+                classNames: ['cardLink']
+            }, [...createLinkIcons(user.contacts)])
         ])
     ]);
 }
 
 
-const socNetworks = new Map(
-    ['www.facebook.com', './assets/icons/facebook.svg']
-    ['twitter.com', './assets/icons/twitter.svg']
-    ['www.instagram.com', './assets/icons/instagram.svg']
-);
 
-const createLinkIcons = (contacts) => {
+
+function createLinkIcons(contacts) {
     const arrayWithIcons = contacts.map((contact) => {
         const { hostname } = new URL(contact);
         if (socNetworks.has(hostname)) {
             const classList = socNetworks.get(hostname);
             const img = document.createElement('img');
-            img.setAttribute('src', scr);
+            img.setAttribute('src', classList);
 
             const a = document.createElement('a');
             a.setAttribute('href', contact);
@@ -53,8 +53,8 @@ const createLinkIcons = (contacts) => {
 }
 
 
-function createCardImage(place) {
-    const { firstName, id } = place;
+function createCardImage(user) {
+    const { firstName, id } = user;
     const imageWrapper = document.createElement('div');
     imageWrapper.setAttribute('id', `wrapper${id}`);
     imageWrapper.classList.add('imageWrapper');
@@ -62,7 +62,7 @@ function createCardImage(place) {
     const initials = document.createElement('div');
     initials.classList.add('imagePlaceholder', 'imagePlacement');
     initials.append(document.createTextNode(firstName[0] || ''));
-    imageWrapper.append(initials, createImage(place));
+    imageWrapper.append(initials, createImage(user));
     return imageWrapper;
 }
 
